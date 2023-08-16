dab7ff84634596598ad09c34230474e9355beacc
nitin kukreti <nitin.k@binmile.com>
2023-08-16 18:19:11 +0530

    two function removed and added only one function for handling actions of counter and also define type of action supported by counter using CounterAction enum

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,26 +1,33 @@
 import React, {useState} from 'react';
-import PropTypes from 'prop-types';
+import {CounterAction} from './actions';
 
 const Counter = function () {
-	const [count, setCount] = useState(0);
-	const increment = () => {
-		setCount(count + 1);
-	};
-
-	const decrement = () => {
-		setCount(count - 1);
+	const [count, setCount] = useState<number>(0);
+	const counterActionHandler = function (action: CounterAction) {
+		switch (action) {
+			case CounterAction.Increment:
+				setCount(count + 1);
+				break;
+			case CounterAction.Decrement:
+				setCount(count - 1);
+				break;
+			default:
+				break;
+		}
 	};
 
 	return (
 		<div>
 			<div>Counter-{count}</div>
-			<button onClick={increment}>Increment</button>
-			<button onClick={decrement}>Decrement</button>
+			<button onClick={() => {
+				counterActionHandler(CounterAction.Increment);
+			}}>Increment</button>
+			<button onClick={() => {
+				counterActionHandler(CounterAction.Decrement);
+			}}>Decrement</button>
 
 		</div>
 	);
 };
-
-Counter.propTypes = {};
 
 export default Counter;
