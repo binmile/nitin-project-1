200693ad1313925a6eab54fa3a68613b7d7daf40
nitin kukreti <nitin.k@binmile.com>
2023-08-17 14:05:58 +0530

    destrcture props

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {type PersonInterface} from './interface';
 
-export const Person: React.FC<PersonInterface> = function (props: PersonInterface) {
+export const Person: React.FC<PersonInterface> = function ({age, gender, name, skills}: PersonInterface) {
 	return (
 		<div>
-			<h2>{props.name}</h2>
-			<p>age : {props.age}</p>
-			<p>gender : {props.gender}</p>
-			<p>skills : {props.skills}</p>
+			<h2>{name}</h2>
+			<p>age : {age}</p>
+			<p>gender : {gender}</p>
+			<p>skills : {skills}</p>
 		</div>
 	);
 };
