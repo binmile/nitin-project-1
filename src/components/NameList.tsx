5364cab6dec0d99d38a6f429b8033dc621208f4b
nitin kukreti <nitin.k@binmile.com>
2023-08-30 12:41:07 +0530

    refactor code

--- src/components/NameList.tsx
+++ src/components/NameList.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import {type PersonInterface} from './interface';
 import {Person} from './Person';
+import {names} from '../utils/constants';
 
 export const NameList: React.FC = function () {
-	const names: PersonInterface[] = [{name: 'John', age: 22, gender: 'male', skills: ['React', 'Angular']}, {name: 'Tina', age: 22, gender: 'female', skills: ['Vew', 'Angular']}, {name: 'Tim', age: 22, gender: 'male', skills: ['React', 'Angular', 'node']}];
-	return <div>
-		{names.map((person: PersonInterface, idx: number) => <Person age={person.age} gender={person.gender} name={person.name} skills={person.skills} key={idx} />)} </div>;
+	return (
+		<div>
+			{names.map((person: PersonInterface, idx: number) => (
+				<Person
+					age={person.age}
+					gender={person.gender}
+					name={person.name}
+					skills={person.skills}
+					key={idx}
+				/>
+			))}
+		</div>
+	);
 };
---
+++ src/utils/constants.ts
@@ -0,0 +1,7 @@
+import {type PersonInterface} from '../components/interface';
+
+export const names: PersonInterface[] = [
+	{name: 'John', age: 22, gender: 'male', skills: ['React', 'Angular']},
+	{name: 'Tina', age: 22, gender: 'female', skills: ['Vew', 'Angular']},
+	{name: 'Tim', age: 22, gender: 'male',	skills: ['React', 'Angular', 'node']},
+];
